c81d152a480e5f63afbdff7a8383e9c212bd68e9
rgomez30 <rgomez30@gatech.edu>
2017-11-15 17:20:12 -0500

    Fixed bug because before I was taking all the files under the folder and separated in different folders R script and output.csv

---
+++ data/R_script/merge_data.R
@@ -0,0 +1,36 @@
+#Install required packages
+if (!require(ggplot2)){ 
+  install.packages(ggplot2) 
+} 
+
+if (!require(plyr)){ 
+  install.packages(plyr) 
+} 
+
+if (!require(readr)){ 
+  install.packages(readr) 
+} 
+
+if (!require(dplyr)){ 
+  install.packages(readr) 
+} 
+
+#Read data
+filenames <- list.files(path=getwd(),pattern = "\\.csv$") #List of csv files (just related to matches)
+
+#Transform list of csv files in a list
+All <- lapply(filenames,function(i){
+  i <- paste(i,sep="")
+  read_csv(i)
+})
+filenames <- gsub("-",".",filenames)
+names(All) <- gsub(".csv","",filenames)
+
+#Transform list of csv files on a data frame
+df<-ldply(All, rbind,.id=NULL)
+
+#Save dataframe in just one csv file (output.csv is within /data)
+
+write.csv(df,file="output.csv")
+
+
